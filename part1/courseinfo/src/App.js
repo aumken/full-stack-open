82cf45bfb70285d24ad5cc5d24dc41c34349d64e
aum kendapadi <aum@unc.edu>
2023-03-24 18:17:07 -0400

    1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,12 +1,18 @@
 const App = () => {
   
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name:'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name:'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name:'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => {
     return (
@@ -19,9 +25,9 @@ const App = () => {
   const Content = () => {
     return (
       <div>
-        <Part part={part1} exercise={exercises1}/>
-        <Part part={part2} exercise={exercises2}/>
-        <Part part={part3} exercise={exercises3}/>
+        <Part part={part1.name} exercise={part1.exercises}/>
+        <Part part={part2.name} exercise={part2.exercises}/>
+        <Part part={part3.name} exercise={part3.exercises}/>
       </div>
     )
   }
@@ -46,7 +52,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content />
-      <Total e1={exercises1} e2={exercises2} e3={exercises3}/>
+      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises}/>
     </div>
   )
 }
